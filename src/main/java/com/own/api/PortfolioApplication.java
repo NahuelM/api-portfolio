c26fe3ded6e12e61f939cc84245746cc92199d58
Nahuel <nahuelm210@gmail.com>
2024-01-02 11:32:01 -0300

    add configuration

--- src/main/java/com/own/api/PortfolioApplication.java
+++ src/main/java/com/own/api/PortfolioApplication.java
@@ -2,10 +2,19 @@ package com.own.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
+@Controller
 @SpringBootApplication
 public class PortfolioApplication {
 
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
 	}
