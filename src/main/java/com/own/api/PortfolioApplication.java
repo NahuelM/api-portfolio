205f6854b0f9db691bb483cc7dccdc45a1ae430f
Nahuel <nahuelm210@gmail.com>
2024-01-03 15:13:16 -0300

    modify controller

--- src/main/java/com/own/api/PortfolioApplication.java
+++ src/main/java/com/own/api/PortfolioApplication.java
@@ -2,19 +2,11 @@ package com.own.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@Controller
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class PortfolioApplication {
-
-
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "Hello World!";
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(PortfolioApplication.class, args);
 	}
