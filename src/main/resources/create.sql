9f494908a538651e790c9773faedff8937edd1da
Nahuel <nahuelm210@gmail.com>
2024-01-04 10:15:46 -0300

    add properties and dependency log4j, junit, h2

---
+++ src/main/resources/create.sql
@@ -0,0 +1,4 @@
+CREATE TABLE IF NOT EXISTS Products(id INT auto_increment PRIMARY KEY, name VARCHAR(255), price DOUBLE, description VARCHAR (355), category INT, images[] VARCHAR(400));
+CREATE TABLE IF NOT EXISTS Category(id INT auto_increment PRIMARY KEY, name VARCHAR(255));
+CREATE TABLE IF NOT EXISTS Users(id INT auto_increment PRIMARY KEY, name VARCHAR(255), email VARCHAR (255), password VARCHAR(255), role VARCHAR(255), avatarImg VARCHAR(255));
+CREATE TABLE IF NOT EXISTS Products_Category(idProduct INT, idCategory INT)
